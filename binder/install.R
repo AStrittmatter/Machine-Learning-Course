810608e6293e7e04af74ed80aeacee109ef6c76d
AStrittmatter <anthony.strittmatter@ensae.fr>
2021-05-14 13:18:15 +0200

    update

--- binder/install.R
+++ binder/install.R
@@ -1,5 +1,6 @@
 install.packages('corrplot')
 install.packages('glmnet')
+install.packages('DiagrammeR')
 install.packages('grf')
 install.packages('dplyr')
 install.packages('plotmo')
@@ -7,5 +8,5 @@ install.packages('AER')
 install.packages('fBasics')
 install.packages('tidyverse')
 install.packages('rpart.plot')
-install.packages('DiagrammeR')
+
 
